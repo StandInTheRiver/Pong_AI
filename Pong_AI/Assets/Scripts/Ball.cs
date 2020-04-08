7fc890d5649daf70da05a2f95b947dc8512060a0
Thadchayani Kupendiran <thadcha25@hotmail.com>
2020-04-08 17:22:48 -0400

    pong game
    completed pong game with movement
    
    (remaining: preventing the paddles from leaving the camera view, accelerated collision when the ball hits the corner)

---
+++ Pong_AI/Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float startX = Random.Range(0, 2) == 0 ? -1 : 1;
+        float startY = Random.Range(0, 2) == 0 ? -1 : 1;
+
+        GetComponent<Rigidbody>().velocity = new Vector3(speed * startX, speed * startY, 0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
