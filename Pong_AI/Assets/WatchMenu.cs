481e2af4a96377c257a6aa5559d8b7230c432bad
Thadchayani Kupendiran <thadcha25@hotmail.com>
2020-04-19 18:32:00 -0400

    menu added

---
+++ Pong_AI/Assets/WatchMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WatchMenu : MonoBehaviour
+{
+    public void WatchOffOff()
+    {
+        SceneManager.LoadScene("offvoff");
+    }
+
+    public void WatchDefOff()
+    {
+        SceneManager.LoadScene("offvsdef");
+    }
+
+    public void WatchDefDef()
+    {
+        SceneManager.LoadScene("defvsdef");
+    }
+}
